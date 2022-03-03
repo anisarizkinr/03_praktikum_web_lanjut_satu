b80b0f97cbc737fbe83f8336096b8adef4ed4fc0
anisarizkinr <nurrahma9b07.sooko1@gmail.com>
2022-03-03 21:32:53 +0700

    memperbaiki link href

--- resources/views/blog/header.blade.php
+++ resources/views/blog/header.blade.php
@@ -5,25 +5,25 @@
     <header class="">
       <nav class="navbar navbar-expand-lg">
         <div class="container">
-          <a class="navbar-brand" href="blog/index"><h2>Sixteen <em>Clothing</em></h2></a>
+          <a class="navbar-brand" href="/index"><h2>Sixteen <em>Clothing</em></h2></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="blog/index">Home
+                <a class="nav-link" href="/index">Home
                   <span class="sr-only">(current)</span>
                 </a>
               </li> 
               <li class="nav-item">
-                <a class="nav-link" href="blog/product">Our Products</a>
+                <a class="nav-link" href="/product">Our Products</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="blog/about">About Us</a>
+                <a class="nav-link" href="/about">About Us</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="blog.contact">Contact Us</a>
+                <a class="nav-link" href="/contact">Contact Us</a>
               </li>
             </ul>
           </div>
