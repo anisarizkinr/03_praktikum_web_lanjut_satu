481678131cb63a175a9a3092053ef7364f1afd2e
anisarizkinr <nurrahma9b07.sooko1@gmail.com>
2022-03-08 22:58:18 +0700

    Membuat Model

---
+++ database/migrations/2022_03_08_145247_create_produks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('Produk', 100);
+            $table->string('Gambar', 255);
+            $table->text('content');
+            $table->string('Harga', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produks');
+    }
+}
